1ca7442e6ab2e0d0482357f545fad471534536f0
A.M. Knight <aknight@gmail.com>
2013-03-23 20:20:08 -0400

    first model sketch plus skeleton

---
+++ app/models/Cheese.java
@@ -0,0 +1,35 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+
+import play.db.ebean.*;
+import play.data.format.*;
+import play.data.validation.*;
+
+@Entity
+class Cheese extends Model {
+	@Id
+	@Constraints.Min(10)
+	public Long id;
+
+	@Constraints.Required
+	public String name;
+
+	public boolean finished;
+
+	@Formats.DateTime(pattern="dd/MM/yyyy")
+	public Date startDate;
+
+	@OneToOne
+	public Cave cave;
+
+	public String cheeseStyle;
+	public String recipeSource;
+	public String coagulant;
+	public String 
+
+	public static Finder<Long,Cheese> find = 
+			new Finder<Long,Cheese>(Long.class, Cheese.class);
+
+}
