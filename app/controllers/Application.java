654d84317bd2cb6b3e6be3bb0683ab689a4758b1
A.M. Knight <aknight@gmail.com>
2013-04-08 10:15:17 -0400

    registration works! other things don't...

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,14 +2,23 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.validation.Constraints.*;
+import play.data.*;
+
+import static play.data.Form.*;
 
 import views.html.*;
 
 import models.*;
 
 public class Application extends Controller {
+
+    public setTab() {
+        session("tab", "home");
+    }
   
     public static Result index() {
+        setTab();
         return ok(index.render("Blessed are the Cheesemakers", 
                     session("email") != null ? true : false));
     }
@@ -28,20 +37,54 @@ public class Application extends Controller {
         
     }
 
+    public static class Register {
+        @Required
+        public String email;
+        @Required
+        public String name;
+        @Required
+        public String password;
+    }
+
     /**
      * Login page.
      */
     public static Result login() {
+        setTab();
         // return ok(
         //     login.render(form(Login.class))
         // );
         return new Results.Todo();
+    }
+
+    public static Result registration() {
+        setTab();
+        return ok(
+            views.html.register.render(form(Register.class))
+        );
+    }
+
+    public static Result register() {
+        setTab();
+        Form<Register> registrationForm = form(Register.class);
+        Form<Register> filledForm = registrationForm.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(register.render(filledForm));
+        } else {
+            User newUser = new User(filledForm.get().email, 
+                                    filledForm.get().name, 
+                                    filledForm.get().password);
+            newUser.save();
+            session("email", filledForm.get().email);
+            return redirect( routes.Application.index() );
+        }        
     }
     
     /**
      * Handle login form submission.
      */
     public static Result authenticate() {
+        setTab();
         // Form<Login> loginForm = form(Login.class).bindFromRequest();
         // if(loginForm.hasErrors()) {
         //     return badRequest(login.render(loginForm));
@@ -58,6 +101,7 @@ public class Application extends Controller {
      * Logout and clean the session.
      */
     public static Result logout() {
+        setTab();
         session().clear();
         flash("success", "You've been logged out");
         return redirect(
