e108af984330f94b025ca8b6577fb91882c0a287
ilkelma <aknight@gmail.com>
2013-04-18 23:42:09 -0400

    Whoa, adding a new cheese works now!

--- app/controllers/Cheeses.java
+++ app/controllers/Cheeses.java
@@ -6,7 +6,11 @@ import play.libs.Json;
 import org.codehaus.jackson.*;
 import play.mvc.*;
 import java.util.*;
+import java.io.File;
 import play.data.validation.Constraints.*;
+import play.data.*;
+import play.mvc.Http.*;
+import play.mvc.Http.MultipartFormData.*;
 
 import static play.data.Form.*;
 
@@ -14,6 +18,9 @@ import views.html.*;
 import models.*;
 
 public class Cheeses extends Controller {
+
+    public static final String PAGE_ADD_CHEESE = "Add Cheese";
+    public static final String PAGE_CHEESES = "Cheeses";
 
     public static class AddCheese {
 
@@ -49,11 +56,11 @@ public class Cheeses extends Controller {
             List<Cheese> cheeses = 
                     Cheese.find.where().eq("user.email", session("email")).findList();
             User user = User.find.byId(session("email"));
-            return ok(views.html.cheeses.cheeseIndex.render("Cheeses", cheeses, user));
+            return ok(views.html.cheeses.cheeseIndex.render(PAGE_CHEESES, cheeses, user));
         } else {
             List<Cheese> cheeses = 
                     Cheese.find.where().eq("visible", true).findList();
-            return ok(views.html.cheeses.cheeseIndex.render("Cheeses", cheeses, null));
+            return ok(views.html.cheeses.cheeseIndex.render(PAGE_CHEESES, cheeses, null));
         }
     }
 
@@ -72,13 +79,60 @@ public class Cheeses extends Controller {
     }
 
     public static Result addCheese() {
-        return ok(views.html.cheeses.newCheese.render("Add Cheese", form(AddCheese.class),
+        return ok(views.html.cheeses.newCheese.render(PAGE_ADD_CHEESE, form(AddCheese.class),
                     CheeseStyle.find.all(), MilkType.find.all(),
                     Cave.find.where().eq("owner.email", session("email")).findList()));
     }
 
     public static Result create() {
-    	return new Results.Todo();
+        Form<AddCheese> cheeseForm = form(AddCheese.class);
+        Form<AddCheese> filledForm = cheeseForm.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(views.html.cheeses.newCheese.render(PAGE_ADD_CHEESE, filledForm,
+                    CheeseStyle.find.all(), MilkType.find.all(),
+                    Cave.find.where().eq("owner.email", session("email")).findList()));
+        } else {
+            MultipartFormData body = request().body().asMultipartFormData();
+            FilePart image = body.getFile("image");
+            Cheese newCheese = new Cheese(filledForm.get().name);
+            // Set required fields
+            newCheese.setUser(User.find.byId(session("email")));
+            newCheese.setFinished(filledForm.get().finished);
+            newCheese.setVisible(filledForm.get().visible);
+            newCheese.setCheeseStyle(filledForm.get().cheeseStyle);
+            newCheese.setMilkType(filledForm.get().milkType);
+            // Set nullable fields
+            if(filledForm.get().startDate != null) {
+                newCheese.setStartDate(filledForm.get().startDate);
+            }
+            if(filledForm.get().finishDate != null) {
+                newCheese.setFinishDate(filledForm.get().finishDate);
+            }
+            if(filledForm.get().cave != null) {
+                newCheese.setCave(Cave.find.byId(filledForm.get().cave));
+            }
+            if(filledForm.get().recipeSource != null) {
+                newCheese.setRecipeSource(filledForm.get().recipeSource);
+            }
+            if(filledForm.get().coagulant != null) {
+                newCheese.setCoagulant(filledForm.get().coagulant);
+            }
+            if(filledForm.get().inoculant != null) {
+                newCheese.setInoculant(filledForm.get().inoculant);
+            }
+            // Set image path and save image if uploaded
+            if(image != null) {
+                newCheese.setImage(image.getFilename());
+                File file = image.getFile();
+                // Save file here
+            } else {
+                newCheese.setImage("default.png");
+            }
+            // Persist the cheese!
+            newCheese.save();
+
+            return redirect(routes.Cheeses.index());
+        }        
     }
   	
     public static Result update(Long cheese) {
--- app/models/Cheese.java
+++ app/models/Cheese.java
@@ -72,4 +72,105 @@ public class Cheese extends Model {
 		return theDate.toString();
 	}
 
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public boolean getFinished(){
+		return finished;
+	}
+
+	public void setFinished(boolean finished){
+		this.finished = finished;
+	}
+
+	public Date getStartDate(){
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate){
+		this.startDate = startDate;
+	}
+	public Date getFinishDate(){
+		return finishDate;
+	}
+
+	public void setFinishDate(Date finishDate){
+		this.finishDate = finishDate;
+	}
+	public boolean getVisible(){
+		return visible;
+	}
+
+	public void setVisible(boolean visible){
+		this.visible = visible;
+	}
+	public Cave getCave(){
+		return cave;
+	}
+
+	public void setCave(Cave cave){
+		this.cave = cave;
+	}
+	public User getUser(){
+		return user;
+	}
+
+	public void setUser(User user){
+		this.user = user;
+	}
+	public String getCheeseStyle(){
+		return cheeseStyle;
+	}
+
+	public void setCheeseStyle(String cheeseStyle){
+		this.cheeseStyle = cheeseStyle;
+	}
+	public String getRecipeSource(){
+		return recipeSource;
+	}
+
+	public void setRecipeSource(String recipeSource){
+		this.recipeSource = recipeSource;
+	}
+	public String getCoagulant(){
+		return coagulant;
+	}
+
+	public void setCoagulant(String coagulant){
+		this.coagulant = coagulant;
+	}
+	public String getInoculant(){
+		return inoculant;
+	}
+
+	public void setInoculant(String inoculant){
+		this.inoculant = inoculant;
+	}
+	public String getMilkType(){
+		return milkType;
+	}
+
+	public void setMilkType(String milkType){
+		this.milkType = milkType;
+	}
+	public String getImage(){
+		return image;
+	}
+
+	public void setImage(String image){
+		this.image = image;
+	}
 }
